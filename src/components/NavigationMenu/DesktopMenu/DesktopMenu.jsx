1d62e45e2daf167ec08289d6d69f65e2472c4419
Lloyd Chambrier <lloydchambrier@gmail.com>
2022-05-29 10:50:15 +0000

    chore:articles json files add

--- src/components/NavigationMenu/DesktopMenu/DesktopMenu.jsx
+++ src/components/NavigationMenu/DesktopMenu/DesktopMenu.jsx
@@ -1,16 +1,43 @@
 import { ReactComponent as WhiteDeptIcon } from '../../../assets/icons/DEPT-white-icon.svg'
 import { ReactComponent as ClosingIcon } from '../../../assets/icons/closing-white-icon.svg';
+import { ReactComponent as ArrowIcon } from '../../../assets/icons/polygon-landen-icon.svg';
 import './DesktopMenu.scss';
 
 const DesktopMenu = ({ boxVisibility, setBoxVisibility }) => {
     const menuItems = ["Home","Work", "Culture", "Services","Partners", "Stories", "Careers", "Events", "Contact"];
+    const locales = ["Global", "Nederland", "United States", "Ireland", "United Kingdom", "Deutschland"];
     return (
-        <div style ={{ height:'100vh',display: 'flex', flexDirection: 'column', background: 'black', paddingRight: '1.6rem'}}>
+        <div className='desktop-menu'>
             <div className='desktop-container'>
-                <WhiteDeptIcon /><ClosingIcon onClick={()=> setBoxVisibility(!boxVisibility)}/>
+                <div style={{ width: '20.7rem', height: '16.8rem', display: 'flex', flexDirection: 'column', zIndex: '3'}}>
+                    <div style={{}}>
+                        <WhiteDeptIcon />
+                        <div className='landen'>
+                            <h2>Landen</h2>
+                            <br/>
+                            <ul className='locale-container'>
+                                {locales.map((locale, i) => {
+                                    if(i === 1){
+                                        return <li key={i} className='locale-item'><ArrowIcon/> <span>{locale}</span></li>
+                                    }
+                                    return <li key={i} className='locale-item'>{locale}</li>
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <button>
+                    <ClosingIcon onClick={()=> setBoxVisibility(!boxVisibility)}/>
+                </button>
             </div>
             <ul className='list-menu-desktop'>
-                {menuItems.map((item, i) => <li key={i} className='list-item-menu-desktop'>{item}</li>)}
+                {menuItems.map((item, i) =>{
+                    if(i === 0){
+                        return(<li key={i} className='list-item-menu-desktop first-item-mobile'><ArrowIcon style={{ marginRight: '10.5px'}} /><span>{item}</span></li>)
+                    }
+                    return  <li key={i} className='list-item-menu-desktop'>{item}</li>;
+                    })
+                }
             </ul>
         </div>);
 }
